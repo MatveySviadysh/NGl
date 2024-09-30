eae2de8b9d7a243b2413ac6d0c9fe13a190e1280
MatveySviadysh <mkomp06@gmail.com>
2024-09-30 21:58:51 +0300

    create register & login logout

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register, user_login, profile, user_logout
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('profile/', profile, name='profile'),
+]
---
+++ user/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import login, logout
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('profile')
+    else:
+        form = UserLoginForm()
+    return render(request, 'user/login.html', {'form': form})
+
+def profile(request):
+    return render(request, 'user/profile.html', {'user': request.user})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
