62a9e399f0b956140a88180eef7b4667dbf6d753
MatveySviadysh <mkomp06@gmail.com>
2024-10-07 07:29:15 +0300

    fix my bot location folders and files

--- mybot/urls.py
+++ mybot/urls.py
@@ -1,8 +1,6 @@
-# mybot/urls.py
 from django.urls import path
-from .views import SendMessageView, ButtonPageView
+from .views import ButtonPageView
 
 urlpatterns = [
-    path('send-message/', SendMessageView.as_view(), name='send_message'),
-    path('button/', ButtonPageView.as_view(), name='button_page'),  # Add this line for the button page
+    path('button/', ButtonPageView.as_view(), name='button_page'),
 ]
