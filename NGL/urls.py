ff6efc3685d50ff232bf03769113dc7c03f7cb20
MatveySviadysh <mkomp06@gmail.com>
2024-10-12 23:10:39 +0300

    create new app order

--- NGL/urls.py
+++ NGL/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/user/main/', permanent=False)), 
     path('user/', include('user.urls')),
     path('mybot/', include('mybot.urls')),
+    path('order/', include('order.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
