664a32daefad0566e50b252973d2eec74b23ef28
MatveySviadysh <mkomp06@gmail.com>
2024-10-14 22:17:17 +0300

    crete get aswer to find tutors

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('create/', create_order, name='create_order'),
+    path('answer/', answer, name='answer'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from user.models import Tutor
 from .models import UserConsultation
 from .forms import UserConsultationForm
+
+def answer(request):
+    user_consultation = UserConsultation.objects.last()
+
+    tutors = Tutor.objects.filter(
+        specialization__icontains=user_consultation.specialization,
+    )[:10]
+
+    context = {
+        'tutors': tutors,
+        'user_consultation': user_consultation,
+    }
+
+    return render(request, 'order/pages/AnswerPage.html', context)
 
 @login_required
 def create_order(request):
@@ -9,7 +25,7 @@ def create_order(request):
         form = UserConsultationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('main-page')
+            return redirect('answer')
     else:
         form = UserConsultationForm()
     return render(request, 'order/pages/CreateOrder.html', {'form': form})
