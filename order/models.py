fe575dffa4d45a7761fb289b07c6509c87df3f11
MatveySviadysh <mkomp06@gmail.com>
2024-10-14 22:16:46 +0300

    update model order

--- order/models.py
+++ order/models.py
@@ -30,6 +30,7 @@ class UserConsultation(models.Model):
     meeting_format = models.CharField(max_length=10, choices=MEETING_FORMAT_CHOICES)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     comments = models.TextField(blank=True)
+    specialization = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.target_audience} - {self.age} лет"
