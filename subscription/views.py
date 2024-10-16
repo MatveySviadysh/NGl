1b8cce3786500a34af84a69045b78d3e199324c1
MatveySviadysh <mkomp06@gmail.com>
2024-10-16 14:26:48 +0300

    create page my subscriptions

--- subscription/views.py
+++ subscription/views.py
@@ -19,3 +19,10 @@ def unsubscribe(request, tutor_id):
     tutor = Tutor.objects.get(id=tutor_id)
     Subscription.objects.filter(user=request.user, tutor=tutor).delete()
     return redirect('profile-tutor',tutor.full_name)
+
+@login_required(login_url='login-user')
+def user_subscriptions(request):
+    subscriptions = Subscription.objects.filter(user=request.user).select_related('tutor')
+    return render(request, 'subscription/UserSubscriptions.html', {
+        'subscriptions': subscriptions
+    })
