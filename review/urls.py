965c0a4cc32c4044dcda8d2bb38ba8093052a77f
MatveySviadysh <mkomp06@gmail.com>
2024-10-22 13:31:26 +0300

    show all reviews to tutor

--- review/urls.py
+++ review/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('tutor/<int:tutor_id>/comment/', views.add_comment, name='add_comment'),
-
+    path('tutor_review/', views.ALLReviewTutor, name='all_review_tutor'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,41 +1,29 @@
 from django.shortcuts import redirect, render
 from review.forms import CommentForm
 from user.models import Tutor
+from review.models import Comment
+from django.core.paginator import Paginator
 
 def add_comment(request, tutor_id):
-
     tutor = Tutor.objects.get(id=tutor_id)
-
-    
-
     if request.method == 'POST':
-
         form = CommentForm(request.POST)
-
         if form.is_valid():
-
             comment = form.save(commit=False)
-
             comment.tutor = tutor
-
             comment.user = request.user
-
             comment.save()
-
-            # Redirect to the profile page without changing it
-
             return redirect('profile-tutor', full_name=tutor.full_name)
-
     else:
-
         form = CommentForm()
-
-    
-
     return render(request, 'review/add_comment.html', {
-
         'form': form,
-
         'tutor': tutor
-
     })
+
+def ALLReviewTutor(request):
+    all_reviews = Comment.objects.all()
+    paginator = Paginator(all_reviews, 10)  # 10 reviews per page
+    page_number = request.GET.get('page')  # Get the page number from the request
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'review/ALLReviewTutor.html',{'all_reviews':all_reviews ,'page_obj': page_obj})
