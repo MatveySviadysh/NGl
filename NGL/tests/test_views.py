4b678ae1954fb5a3712c2a5ae60dcb2c49dcbdc6
MatveySviadysh <mkomp06@gmail.com>
2024-11-09 22:27:19 +0300

    fix merge conflict test file 2 try

--- NGL/tests/test_views.py
+++ NGL/tests/test_views.py
@@ -702,7 +702,6 @@ def test_about_company_view(client):
 
 User = get_user_model()
 
-
 @pytest.mark.django_db
 def test_tutor_list_view(client, create_tutors):
     client.login(username='tutor1', password='qwe1234QWER')
